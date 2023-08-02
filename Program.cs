2cfe275c2b2d21d8d027a64f12298cb5a59262b1
Mithlesh Kumar Gupta <84225437+mithleshgupta@users.noreply.github.com>
2023-08-02 08:53:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FileUploadConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the Main method of FileUploadExample class
+            FileUploadExample.Main(args);
+        }
+    }
+}
